9cadabb48c3b9e5657e6707183865db05ea615fd
yang <yang@qq.com>
2020-09-28 10:01:41 +0800

    first commit

---
+++ sp05-eureka/src/test/java/com/tedu/sp05/Sp05EurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.tedu.sp05;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp05EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
